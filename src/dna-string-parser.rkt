c1f1337dc759df7d06a83c1f8942f299478531a4
Anthony Ha <awfa96@cs.washington.edu>
2017-02-20 19:19:13 -0800

    Update syntax to structs and add pretty printing

--- src/dna-string-parser.rkt
+++ src/dna-string-parser.rkt
@@ -1,63 +1,136 @@
 #lang rosette
 
-(provide string->species)
+(require "dna-syntax.rkt")
 
-; Attempts to parse the given input into the representation defined
-; by the grammar in dna-syntax.rkt
-; Undefined on input that doesn't follow the syntax described in
-; "Abstractions for DNA circuit design" paper (as in, it can do anything)
-(define (string->species input)
+(provide
+ ; Returns a dna struct that represents the input string.
+ ; Will match the semantics of the string format as
+ ;   described in "Abstractions for DNA circuit design".
+ ; The behavior is UNDEFINED on input that doesn't match the said string format.
+ ; Mutates the given symbol-table (see parameters section for more info)
+ ;
+ ; -- Parameters --
+ ; input - A string in the format mentioned above to turn into a dna struct.
+ ; symbol-table - A mutable mapping from names that appear in the input to integers
+ ;                  representing them.
+ ;                If there isn't a mapping for a name, a mapping will be added
+ ;                  such that the name will map to an integer that is unique from
+ ;                  all the other integers mapped in the symbol-table.
+ ;                By default uses a default global symbol table which is the same one
+ ;                  that species->string uses
+ string->species
+         
+ ; Returns a string that represents the input dna struct.
+ ; Behaves as an inverse to string->species so will the result will
+ ;   be in the string format as described in "Abstractions for DNA circuit design"
+ ;
+ ; -- Parameters --
+ ; input - A valid dna struct representing the species to turn into a string.
+ ;         Errors if not input isn't a valid dna struct
+ ; symbol-table - A 1-1 mapping between friendly name strings to integers.
+ ;                Every integer in input will be represented in the result
+ ;                  as the friendly name string that maps to that integer.
+ ;                Errors if an integer in input has no corresponding name.
+ ;                By default uses the same default global symbol table as string->species.
+ species->string
+)
+
+; The default symbol table used by string->species and species->string
+; These symbol tables map strings to integers
+; The strings are the friendly names and the integers are 
+(define default-symbol-table (make-hash))
+
+(define (species->string input [symbol-table default-symbol-table])
+  (define inverse-symbol-table (make-hasheq)) ; inverse hash table will let us lookup id to get pretty string
+  (define (lookup id) (hash-ref inverse-symbol-table id
+                                    (thunk (error "Id not found:" id))))
+  (define (to-string input)
+    (match input
+      [(toehold id) (string-append (to-string id) "^")]
+      [(complement id-or-toehold) (string-append (to-string id-or-toehold) "*")]
+      [(upper-strand domain-list) (string-append "<" (to-string domain-list) ">")]
+      [(lower-strand domain-list) (string-append "{" (to-string domain-list) "}")]
+      [(duplex-strand domain-list) (string-append "[" (to-string domain-list) "]")]
+      [(gate lu ll d rl ru)
+       (let ([filter (lambda (s) (if (= (string-length s) 2) "" s))]) ; filter out <>, {}, and []
+         (string-append (filter (to-string lu))
+                        (filter (to-string ll))
+                        (filter (to-string d))
+                        (filter (to-string rl))
+                        (filter (to-string ru))))]
+      [(gate: g1 g2) (string-append (to-string g1) ":" (to-string g2))]
+      [(gate:: g1 g2) (string-append (to-string g1) "::" (to-string g2))]
+      [(list domains ...) (string-join (map to-string domains) " ")]
+      [_ (lookup input)]))
+  (begin
+    (hash-for-each symbol-table
+                   (lambda (str id)
+                     (hash-set! inverse-symbol-table id str)))
+    (if (valid-dna-struct? input)
+        (to-string input)
+        (error "Given invalid dna-struct"))))
+
+(define (string->species input [symbol-table default-symbol-table])
   (cond
     [(string-contains? input "::") ; upper strand gate concatenation
      (let ([split-result (string-split input "::")])
        (foldl (lambda (substring accumulator)
-                (list `:: accumulator (string->species substring)))
-              (string->species (car split-result)) (cdr split-result)))]
+                (gate:: accumulator (string->species substring symbol-table)))
+              (string->species (car split-result) symbol-table) (cdr split-result)))]
     [(string-contains? input ":") ; lower strand gate concatenation
      (let ([split-result (string-split input ":")])
        (foldl (lambda (substring accumulator)
-                (list `: accumulator (string->species substring)))
-              (string->species (car split-result)) (cdr split-result)))]
+                (gate: accumulator (string->species substring symbol-table)))
+              (string->species (car split-result) symbol-table) (cdr split-result)))]
     [(string-contains? input "[") ; indicates complex (gate)
-     (parse-gate-string input)]
+     (parse-gate-string input symbol-table)]
     [(string-contains? input "<") ; indicates upper strand
-     (parse-upper-strand-string input)]
+     (parse-upper-strand-string input symbol-table)]
     [(string-contains? input "{") ; indicates lower strand
-     (parse-lower-strand-string input)]
+     (parse-lower-strand-string input symbol-table)]
     ))
 
-(define (parse-sequence-string input)
+(define (parse-sequence-string input symbol-table)
   (let ([is-complement (string-contains? input "*")]
         [is-toehold (string-contains? input "^")]
-        [symbol (string->symbol (string-trim input (regexp "\\*|\\^") #:repeat? #t))])
+        [name (string-trim input (regexp "\\*|\\^") #:repeat? #t)])
+    (define symbol
+      (if (hash-has-key? symbol-table name)
+          (hash-ref symbol-table name)
+          ; creates a new-id to be one higher than the max integer in the symbol-list
+          (let ([new-id (+ 1
+                           (argmax identity (cons -1 (hash-values symbol-table)))
+                           )])
+            (hash-set! symbol-table name new-id)
+            new-id)))
     (define inner-result (if is-toehold
-                             `(T ,symbol)
-                             `(S ,symbol)))
+                             (toehold symbol)
+                             symbol))
     (if is-complement
-        `(C ,inner-result)
+        (complement inner-result)
         inner-result)
     ))
 
-(define (parse-domain-string input)
-  (filter (lambda (e) (not (null? e))) (map parse-sequence-string (string-split input))))
+(define (parse-domain-string input symbol-table)
+  (filter (lambda (e) (not (null? e))) (map (lambda (input) (parse-sequence-string input symbol-table)) (string-split input))))
 
-(define (parse-upper-strand-string input)
-  (list `U (parse-domain-string (string-trim input (regexp "<|>")))))
+(define (parse-upper-strand-string input symbol-table)
+  (upper-strand (parse-domain-string (string-trim input (regexp "<|>")) symbol-table)))
 
-(define (parse-lower-strand-string input)
-  (list `L (parse-domain-string (string-trim input (regexp "{|}")))))
+(define (parse-lower-strand-string input symbol-table)
+  (lower-strand (parse-domain-string (string-trim input (regexp "{|}")) symbol-table)))
 
-(define (parse-gate-string input)
+(define (parse-gate-string input symbol-table)
   (let* ([components (string-split input (regexp "\\[|\\]") #:trim? #f)]
          [left-side (list-ref components 0)]
          [complex (list-ref components 1)]
          [right-side (list-ref components 2)])
-    (list `gate
-          (parse-upper-strand-string (car (or (regexp-match (regexp "<.*>") left-side) `(""))))
-          (parse-lower-strand-string (car (or (regexp-match (regexp "{.*}") left-side) `(""))))
-          (parse-domain-string complex)
-          (parse-lower-strand-string (car (or (regexp-match (regexp "{.*}") right-side) `(""))))
-          (parse-upper-strand-string (car (or (regexp-match (regexp "<.*>") right-side) `(""))))
+    (gate
+          (parse-upper-strand-string (car (or (regexp-match (regexp "<.*>") left-side) `(""))) symbol-table)
+          (parse-lower-strand-string (car (or (regexp-match (regexp "{.*}") left-side) `(""))) symbol-table)
+          (duplex-strand (parse-domain-string complex symbol-table))
+          (parse-lower-strand-string (car (or (regexp-match (regexp "{.*}") right-side) `(""))) symbol-table)
+          (parse-upper-strand-string (car (or (regexp-match (regexp "<.*>") right-side) `(""))) symbol-table)
           )
     )
   )
--- src/dna-syntax.rkt
+++ src/dna-syntax.rkt
@@ -1,85 +1,87 @@
-#lang rosette
+#lang rosette/safe
 
-; This file provides predicates that are able to check whether
-; a given input corresponds to a non-terminal in our defined
-; DNA strand displacement grammar
+(require rosette/lib/match)
 
-(provide id?
-         sequence?
-         domain?
-         upper?
-         lower?
-         strand?
-         gate?
-         species?
-         system?)
+(provide
+ ; domains are toeholds, complements, or ids ('integer?'s)
+ (struct-out toehold)       ; toehold struct       - takes an id
+ (struct-out complement)    ; complement struct    - takes a id or toehold
+ (struct-out upper-strand)  ; upper-strand struct  - takes a list of domains
+ (struct-out lower-strand)  ; lower-strand struct  - takes a list of domains
+ (struct-out duplex-strand) ; duplex-strand struct - takes a list of domains
+ (struct-out gate)          ; gate struct          - takes an upper-strand, lower-strand, duplex, lower-strand, and upper-strand
+ (struct-out gate:)         ; gate: struct         - takes two gates
+ (struct-out gate::)        ; gate:: struct        - takes two gates
+ valid-dna-struct? ; returns #t iff the input is a valid dna struct, #f otherwise
+                   ;  a valid dna struct is a dna struct (toehold, complement,
+                   ;   upper-strand, lower-strand, duplex-strand, gate, gate:, gate::)
+                   ;  and the dna struct is constructed only from valid inputs as described above
+)
 
-; Syntax of the DNA strand displacement language
-; <id>       := NAME
-;
-; <sequence> := (S <id>)
-;             | (T <id>)
-;             | (C (S <id>))
-;             | (C (T <id>))
-;
-; <domain>   := (<sequence>*)
-;
-; <upper>    := (U domain)
-; <lower>    := (L domain)
-;
-; <strand>   := upper
-;             | lower
-;
-; <gate>     := (gate <upper> <lower> <domain> <lower> <upper>)
-;             | (: <gate> <gate>)
-;             | (:: <gate> <gate>)
-;
-; <species>  := <strand>
-;             | <gate>
-;
-; <system>   := (<species>+)
+; Checking for equality between ids are all we need to implement
+; DNA strand manipulation rules and integers support equality checking
+; ids that are not equal represent distinct dna strands
+; that won't interact with eachother.
 
-(define id? symbol?)
+; ids are considered 'domain's
+; toehold and complement are also 'domain's
+(struct toehold (id) #:transparent)               ; Represents a toehold as described by this id
+(struct complement (id-or-toehold) #:transparent) ; Represents the complement of an id or toehold
 
-(define (sequence? input)
-  (match input
-    [`(S ,id) (id? id)]
-    [`(T ,id) (id? id)]
-    [`(C (S ,id)) (id? id)]
-    [`(C (T ,id)) (id? id)]
+; domain-list is a list of domains which can be
+;   - ids
+;   - toeholds (of ids)
+;   - complements (of ids or toeholds of ids)
+(struct upper-strand (domain-list) #:transparent)
+(struct lower-strand (domain-list) #:transparent)
+(struct duplex-strand (domain-list) #:transparent)
+
+; gate, gate: and gate:: are all gates
+(struct gate (left-upper  ; an upper-strand
+              left-lower  ; a lower-strand
+              duplex      ; a duplex
+              right-lower ; a lower-strand
+              right-upper ; an upper-strand
+              ) #:transparent)
+(struct gate: (g1 g2) #:transparent) ; g1 and g2 are gates
+(struct gate:: (g1 g2) #:transparent) ; g1 and g2 are gates
+
+(define (gate-struct? g)
+  (or (gate? g)
+      (gate:? g)
+      (gate::? g)))
+
+(define (domain? dna-struct)
+  (or (toehold? dna-struct)
+      (complement? dna-struct)
+      (integer? dna-struct)))
+
+(define (domain-list? domain-list)
+  (and (list? domain-list) (andmap domain? domain-list)))
+
+(define (valid-dna-struct? dna-struct)
+  (match dna-struct
+    [(toehold id) (integer? id)]
+    [(complement id-or-toehold)
+     (or
+      (integer? id-or-toehold)
+      (and (toehold? id-or-toehold) (valid-dna-struct? id-or-toehold)))]
+    [(upper-strand domain-list) (domain-list? domain-list)]
+    [(lower-strand domain-list) (domain-list? domain-list)]
+    [(duplex-strand domain-list) (domain-list? domain-list)]
+    [(gate lu ll d rl ru)
+     (and
+      (upper-strand? lu) (valid-dna-struct? lu)
+      (lower-strand? ll) (valid-dna-struct? ll)
+      (duplex-strand? d) (valid-dna-struct? d)
+      (lower-strand? rl) (valid-dna-struct? rl)
+      (upper-strand? ru) (valid-dna-struct? ru))]
+    [(gate: g1 g2)
+     (and
+      (gate-struct? g1) (valid-dna-struct? g1)
+      (gate-struct? g2) (valid-dna-struct? g2))]
+    [(gate:: g1 g2)
+     (and
+      (gate-struct? g1) (valid-dna-struct? g1)
+      (gate-struct? g2) (valid-dna-struct? g2))]
     [_ #f]))
-
-(define (domain? input)
-  (and (list? input) (andmap sequence? input)))
-
-(define (upper? input)
-  (match input
-    [`(U ,domain) (domain? domain)]
-    [_ #f]))
-(define (lower? input)
-  (match input
-    [`(L ,domain) (domain? domain)]
-    [_ #f]))
-
-(define (strand? input)
-  (or (upper? input) (lower? input)))
-
-(define (gate? input)
-  (match input
-    [`(gate ,ul ,ll ,domain ,lr ,ur)
-     (upper? ul)
-     (lower? ll)
-     (domain? domain)
-     (lower? lr)
-     (upper? ur)]
-    [`(: ,left-gate ,right-gate)
-     (and (gate? left-gate) (gate? right-gate))]
-    [`(:: ,left-gate ,right-gate)
-     (and (gate? left-gate) (gate? right-gate))]
-    [_ #f]))
-
-(define (species? input)
-  (or (strand? input) (gate? input)))
-
-(define (system? input)
-  (and (list? input) (andmap species? input)))
